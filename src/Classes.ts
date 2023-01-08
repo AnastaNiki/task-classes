c733ba297bd4a5a7d55b831fa8789ae173c4cf2d
AnastaNiki <anasta_niki@mail.ru>
2023-01-08 21:50:43 +0300

    Задание task-classes

--- src/Classes.ts
+++ src/Classes.ts
@@ -3,6 +3,22 @@
 - Значение счётчика хранится в поле counter: number. Значение поля по умолчанию: 0
 - В конструктор можно передать параметр start: number - начальное значение счётчика.
 - Класс должен иметь метод increment, который увеличивает counter на 1 и возвращает результат
-- Класс должен иметь метод decrement, который уменьшает counter на 1 и возвращает результат
+- Класс должен иметь метод increment, который уменьшает counter на 1 и возвращает результат
  */
-export class Counter {}
+export class Counter {
+    counter: number;
+
+    constructor(start: number = 0) {
+        this.counter = start;
+    }
+
+    increment(): number {
+        this.counter += 1;
+        return this.counter;
+    }
+
+    decrement(): number {
+        this.counter -= 1;
+        return this.counter;
+    }
+}
